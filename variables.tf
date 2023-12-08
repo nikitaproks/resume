f6a801927c75f975a15f407a67183ade2d2698b0
Mykyta Prokaiev <mykytaprokaiev@gmail.com>
2023-12-08 22:03:58 +0100

    added terraform

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "linode_token" {
+  description = "Your Linode API Token"
+  type        = string
+}
+
+variable "linode_root_pass" {
+  description = "Root password for the Linode instance"
+  type        = string
+}

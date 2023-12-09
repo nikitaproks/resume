b0bda4c7e77840516283e9d54cf1e67e022ec317
Mykyta Prokaiev <mykytaprokaiev@gmail.com>
2023-12-09 21:49:11 +0100

    prepared for deployment

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "linode_token" {
-  description = "Your Linode API Token"
-  type        = string
-}
-
-variable "linode_root_pass" {
-  description = "Root password for the Linode instance"
-  type        = string
-}
